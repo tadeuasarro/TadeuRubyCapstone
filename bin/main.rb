6aa68679ba55f371c1d5747022290f379da547b6
tadeuasarro <tadeutas@gmail.com>
2020-08-31 13:46:46 -0300

    Add Gemfile and bundle install

--- bin/main.rb
+++ bin/main.rb
@@ -1,12 +1,14 @@
 # /bin/main.rb
 
 require 'nokogiri'
-require 'open-uri'
+require 'httparty'
+require 'byebug'
 
-# Fetch and parse HTML document
-doc = Nokogiri::HTML(URI.open('https://tadeuasarro.web.app'))
-
-puts '### Search for nodes by css'
-doc.css('nav ul.menu li a', 'article h2').each do |link|
-  puts link.content
+def scraper
+  url = 'https://www.microverse.org/'
+  unparsed_page = HTTParty.get(url)
+  parsed_page = Nokogiri::HTML(unparsed_page)
+  byebug
 end
+
+scraper
