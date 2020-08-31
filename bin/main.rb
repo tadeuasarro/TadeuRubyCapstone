c9ae27ca752bd8fae4fd01453a41f3bc25e9de86
tadeuasarro <tadeutas@gmail.com>
2020-08-31 15:24:40 -0300

    Set the scraper to work with github profiles

--- bin/main.rb
+++ bin/main.rb
@@ -5,10 +5,18 @@ require 'httparty'
 require 'byebug'
 
 def scraper
-  url = 'https://www.microverse.org/'
+  url = 'https://github.com/tadeuasarro?tab=repositories'
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
-  byebug
+  repo_listing = parsed_page.css('li.col-12')
+  result_array = []
+  repo_listing.each do |i|
+    repo = {
+      repo_name: i.css('h3.wb-break-all').text.to_s.gsub("\n", '').gsub(' ', ''),
+      last_update: i.css('relative-time.no-wrap').text
+    }
+    puts repo
+  end
+  # byebug
 end
-
 scraper
