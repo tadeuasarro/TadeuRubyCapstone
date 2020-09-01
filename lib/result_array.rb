e9e4f3c625bd8724b7ae6afc993b07e643d50860
tadeuasarro <tadeutas@gmail.com>
2020-09-01 13:11:29 -0300

    Linters run on first PR

--- lib/result_array.rb
+++ lib/result_array.rb
@@ -1,5 +1,4 @@
 class ResultArray < Array
-
   def retrieve_repos(repo_listing)
     repo_listing.each do |i|
       repo = {
@@ -14,12 +13,16 @@ class ResultArray < Array
   end
 
   def display_repos
-    puts "Total repositories found: #{self.count}"
-    self.length.times do |i|
+    puts "Total repositories found: #{count}"
+    length.times do |i|
       puts '----------------'
       puts "Repository name: #{self[i][:repo_name]}."
       puts "Last updated: #{self[i][:last_update]}."
-      puts self[i][:lang_used].to_s.length.zero? ? 'Language used: Not available' : "Language used: #{self[i][:lang_used]}." ;
+      if self[i][:lang_used].to_s.length.zero?
+        puts 'Language used: Not available'
+      else
+        "Language used: #{self[i][:lang_used]}."
+      end
       puts "Number of stars: #{self[i][:star_number]}."
     end
   end
